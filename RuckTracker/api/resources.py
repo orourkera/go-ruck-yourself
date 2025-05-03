c0d58e37e4d3f30384215cdf7bee1c045684802e
Rory <orourkera@gmail.com>
2025-05-03 15:58:00 +0200

    Refactor: Decouple db initialization to fix circular import

--- RuckTracker/api/resources.py
+++ RuckTracker/api/resources.py
@@ -5,7 +5,7 @@ from flask import request
 from flask_restful import Resource
 from sqlalchemy import func, extract
 
-from ..app import db
+from ..extensions import db
 from ..models import User, RuckSession, LocationPoint, SessionReview
 from api.schemas import (
     UserSchema, SessionSchema, LocationPointSchema, 
---
+++ RuckTracker/extensions.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Define the database object
+db = SQLAlchemy()
