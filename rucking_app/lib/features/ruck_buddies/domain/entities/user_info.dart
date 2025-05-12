2d5e836765d38466f5e1bd310aea3f045ef5cd47
Rory <orourkera@gmail.com>
2025-05-12 13:58:52 +0200

    feat: update ruck buddies to use username everywhere; remove displayName; update avatar fallback

--- rucking_app/lib/features/ruck_buddies/domain/entities/user_info.dart
+++ rucking_app/lib/features/ruck_buddies/domain/entities/user_info.dart
@@ -2,23 +2,23 @@ import 'package:equatable/equatable.dart';
 
 class UserInfo extends Equatable {
   final String id;
-  final String displayName;
+  final String username;
   final String? photoUrl;
   
   const UserInfo({
     required this.id,
-    required this.displayName,
+    required this.username,
     this.photoUrl,
   });
   
   factory UserInfo.fromJson(Map<String, dynamic> json) {
     return UserInfo(
       id: json['id']?.toString() ?? '',
-      displayName: json['display_name'] ?? 'Unknown User',
-      photoUrl: json['photo_url'],
+      username: json['username'] ?? 'Unknown User',
+      photoUrl: json['avatar_url'],
     );
   }
   
   @override
-  List<Object?> get props => [id, displayName, photoUrl];
+  List<Object?> get props => [id, username, photoUrl];
 }
