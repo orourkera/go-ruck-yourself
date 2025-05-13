cd13f740351e3007a9b47c9b448ef611886b73a5
Rory <orourkera@gmail.com>
2025-05-13 09:59:22 +0200

    Fix: import flutter/services.dart for HapticFeedback in session controls

--- rucking_app/lib/features/ruck_session/presentation/widgets/session_controls.dart
+++ rucking_app/lib/features/ruck_session/presentation/widgets/session_controls.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rucking_app/shared/theme/app_colors.dart';
 import 'package:rucking_app/shared/theme/app_text_styles.dart';
 
